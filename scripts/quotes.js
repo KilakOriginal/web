8090e9db5175f01bef47912842d80c7ce02dfcab
KilakOriginal <kilakOri@protonmail.com>
2024-09-01 02:50:30 +0200

    Add quote

--- scripts/quotes.js
+++ scripts/quotes.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
             "Strangers passing in the street<br>By chance, two separate glances meet<br>And I am you and what I see is me",
             "(...) [L]et him not vow to walk in the dark, who has not seen the nightfall.",
             "And in the end, the love you take is equal to the love you make.",
+            "Simplicity is the ultimate sophistication.",
             "Nenn es dann, wie du willst,<br>Nenn's Glück! Herz! Liebe! Gott!<br>Ich habe keinen Nahmen<br>Dafür! Gefühl ist alles;<br>Name ist Schall und Rauch,<br>Umnebelnd Himmelsgluth.",
             "Don't let your girlfriend stop you from meeting your wife!",
             "Then, as it was, then again it will be and though the course may change sometimes rivers always reach the sea.",
@@ -15,8 +16,10 @@ document.addEventListener("DOMContentLoaded", function() {
             "Hätte, hätte - Fahraddecke",
             "Wer suchet, der findet; wer drauftritt verschwindet.",
             "I have to tell you now, that no such undertaking has been received and that consequently, this country is at war with Germany.",
-            "The path of the righteous man is beset on all sides by the inequities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will shepherds the weak through the valley of darkness for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who attempt to poison and destroy my brothers. And you will know my name is the Lord when I lay my vengeance upon thee!"
+            "The path of the righteous man is beset on all sides by the inequities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will shepherds the weak through the valley of darkness for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who attempt to poison and destroy my brothers. And you will know my name is the Lord when I lay my vengeance upon thee!",
+            "Great things are not accomplished by those who yield to trends and fads."
             //"His action shows convincingly that there is no chance of expecting that this man will ever give up his practice of using force to gain his will. He can only be stopped by force."
+            //"Never be afraid to fail. It's through failures that we learn and grow." "Leo Fender"
         ],
         "attributions": [
             "Terry Davis",
@@ -24,6 +27,7 @@ document.addEventListener("DOMContentLoaded", function() {
             "Roger Waters",
             "J.R.R. Tolkien/Elrond",
             "Paul McCartney",
+            "Leo Fender",
             "Johann Wolfgang von Goethe",
             "LilyPichu",
             "Jimmy Page",
@@ -33,7 +37,8 @@ document.addEventListener("DOMContentLoaded", function() {
             "Jonathan Apelt",
             "Kampfmittelbeseitigungsdienst Kiel",
             "Neville Chamberlain",
-            "Quentin J. Tarantino"
+            "Quentin J. Tarantino",
+            "Leo Fender"
         ]
     };
 
